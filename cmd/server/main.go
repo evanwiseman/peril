04a6784c9a21c39e14e9c82f52811b3018671139
evanwiseman <evanwiseman2000@gmail.com>
2025-10-10 22:17:13 -0700

    added pubsub and channels

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	log.Println("Starting Peril server...")
+
+	// Start Rabbit MQ using the address
+	const rmqAddr = "amqp://guest:guest@localhost:5672/"
+	rmqConnection, err := amqp.Dial(rmqAddr)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ server.\n")
+	}
+	defer rmqConnection.Close()
+	log.Printf("Successfully connected to Rabbit MQ Server.\n")
+
+	// Open a Rabbit MQ channel
+	rmqChannel, err := rmqConnection.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open RabbitMQ channel.\n")
+	}
+	defer rmqChannel.Close()
+
+	// Publish a JSON message to the exchange
+	pubsub.PublishJSON(
+		rmqChannel,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+
+	// Capture ctrl + c for cleanup
+	c := make(chan os.Signal, 1.)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+	log.Print("Stopping Peril server...")
+	os.Exit(1)
 }
---
+++ internal/pubsub/publish.go
@@ -0,0 +1,36 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	// Marshal val to JSON []byte
+	var data []byte
+	data, err := json.Marshal(val)
+	if err != nil {
+		return fmt.Errorf("failed to marshal val: %v", err)
+	}
+
+	// Publish the message to the exchange
+	err = ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        data,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to publish message to exchange: %v", err)
+	}
+
+	return nil
+}
