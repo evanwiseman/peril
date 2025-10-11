fd63e3d0ee1baacb8c071ce349b5e76e0d8ed111
evanwiseman <evanwiseman2000@gmail.com>
2025-10-11 00:40:13 -0700

    added repl to server and client, cleaned up names

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -13,23 +13,40 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+func cleanup() {
+	log.Print("Stopping Peril client...")
+}
+
 func main() {
+	// Capture ctrl + ctrlC for cleanup
+	ctrlC := make(chan os.Signal, 1.)
+	signal.Notify(ctrlC, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-ctrlC
+		cleanup()
+		os.Exit(1)
+	}()
+
+	// Star the client
 	fmt.Println("Starting Peril client...")
 
-	const url = "amqp://guest:guest@localhost:5672/"
-	conn, err := amqp.Dial(url)
+	// Start Rabbit MQ using the rabbitMQUrl
+	const rabbitMQUrl = "amqp://guest:guest@localhost:5672/"
+	rabbitMQConnection, err := amqp.Dial(rabbitMQUrl)
 	if err != nil {
 		log.Fatalf("Failed to connect to to RabbitMQ server: %v\n", err)
 	}
-	defer conn.Close()
+	defer rabbitMQConnection.Close()
 
+	// Get the username provided by user
 	username, err := gamelogic.ClientWelcome()
 	if err != nil {
 		log.Fatalf("Failed to get username: %v\n", err)
 	}
 
+	// Bind the pause exchange
 	_, _, err = pubsub.DeclareAndBind(
-		conn,
+		rabbitMQConnection,
 		routing.ExchangePerilDirect,
 		fmt.Sprintf("%v.%v", routing.PauseKey, username),
 		routing.PauseKey,
@@ -39,10 +56,39 @@ func main() {
 		log.Fatalf("Failed to declare and bind: %v\n", err)
 	}
 
-	// Capture ctrl + c for cleanup
-	c := make(chan os.Signal, 1.)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-	log.Print("Stopping Peril client...")
-	os.Exit(1)
+	// Start the REPL
+	gs := gamelogic.NewGameState(username)
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "spawn":
+			err = gs.CommandSpawn(words)
+			if err != nil {
+				log.Printf("Failed to spawn unit: %v\n", err)
+			}
+		case "move":
+			_, err := gs.CommandMove(words)
+			if err != nil {
+				log.Printf("Failed to move unit: %v\n", err)
+			} else {
+				log.Printf("Move unit %v to %v successful\n", words[2], words[1])
+			}
+		case "status":
+			gs.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			log.Printf("Spamming not allowed yet!")
+		case "quit":
+			cleanup()
+			os.Exit(1)
+		default:
+			log.Printf("Invalid command: %v.", words[0])
+			continue
+		}
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,52 +1,103 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func main() {
-	log.Println("Starting Peril server...")
+func cleanup() {
+	log.Print("Stopping Peril server...")
+}
 
-	// Start Rabbit MQ using the address
-	const rmqAddr = "amqp://guest:guest@localhost:5672/"
-	rmqConnection, err := amqp.Dial(rmqAddr)
+func main() {
+	// Capture ctrl + ctrlC for cleanup
+	ctrlC := make(chan os.Signal, 1.)
+	signal.Notify(ctrlC, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-ctrlC
+		cleanup()
+		os.Exit(1)
+	}()
+
+	// Start up the server
+	log.Println("Starting Peril server...")
+	gamelogic.PrintServerHelp()
+
+	// Start Rabbit MQ using the rabbitMQUrl
+	const rabbitMQUrl = "amqp://guest:guest@localhost:5672/"
+	rabbitMQConnection, err := amqp.Dial(rabbitMQUrl)
 	if err != nil {
 		log.Fatalf("Failed to connect to RabbitMQ server: %v\n", err)
 	}
-	defer rmqConnection.Close()
+	defer rabbitMQConnection.Close()
 	log.Printf("Successfully connected to Rabbit MQ Server.\n")
 
 	// Open a Rabbit MQ channel
-	rmqChannel, err := rmqConnection.Channel()
+	rabbitMQChannel, err := rabbitMQConnection.Channel()
 	if err != nil {
 		log.Fatalf("Failed to open RabbitMQ channel: %v\n", err)
 	}
-	defer rmqChannel.Close()
+	defer rabbitMQChannel.Close()
 
-	// Publish a JSON message to the exchange
-	err = pubsub.PublishJSON(
-		rmqChannel,
-		routing.ExchangePerilDirect,
-		routing.PauseKey,
-		routing.PlayingState{
-			IsPaused: true,
-		},
+	_, _, err = pubsub.DeclareAndBind(
+		rabbitMQConnection,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug,
+		fmt.Sprintf("%v.*", routing.GameLogSlug),
+		"durable",
 	)
 	if err != nil {
-		log.Fatalf("Faield to publish JSON: %v\n", err)
+		log.Fatalf("Failed to declare and bind: %v\n", err)
 	}
 
-	// Capture ctrl + c for cleanup
-	c := make(chan os.Signal, 1.)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-	log.Print("Stopping Peril server...")
-	os.Exit(1)
+	// Start the REPL
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "pause":
+			log.Println("Sending pause message.")
+			err = pubsub.PublishJSON(
+				rabbitMQChannel,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: true,
+				},
+			)
+			if err != nil {
+				log.Printf("Failed to publish pause message: %v\n", err)
+			}
+		case "resume":
+			log.Println("Sending resume message.")
+			err = pubsub.PublishJSON(
+				rabbitMQChannel,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: false,
+				},
+			)
+			if err != nil {
+				log.Printf("Failed to publish resume message: %v\n", err)
+			}
+		case "quit":
+			cleanup()
+			os.Exit(1)
+		default:
+			log.Printf("Invalid command: %v.", words[0])
+		}
+	}
+
 }
